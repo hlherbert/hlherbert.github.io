c0c808faecaf1a69c6bb81a8e0772fb91b1df582
huangli <184624975@qq.com>
2020-02-12 22:05:35 +0800

    站长统计

--- js/stat.js
+++ js/stat.js
@@ -1,1 +1,5 @@
-<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? "https://" : "http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1278611579'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s9.cnzz.com/z_stat.php%3Fid%3D1278611579%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
+var cnzz_protocol = (("https:" == document.location.protocol) ? "https://" : "http://");
+var now = new Date();
+var year =  now.getFullYear();
+document.write("例如：©2019-" + year +" hlherbert.github.io 版权所有,ICP备:浙B2-2008010")
+document.write(unescape("%3Cspan id='cnzz_stat_icon_1278611579'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s9.cnzz.com/z_stat.php%3Fid%3D1278611579%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));
